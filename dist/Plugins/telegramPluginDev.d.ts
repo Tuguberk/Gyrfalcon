bbf8d654296a96bd539c4c743eb6cdcdd142e207
Tuguberk <akbulut.tugberk@gmail.com>
2025-05-01 17:28:17 +0300

    removed unnecessary files

--- dist/Plugins/telegramPluginDev.d.ts
+++
@@ -1,7 +0,0 @@
-import TelegramPlugin from "@virtuals-protocol/game-telegram-plugin";
-import { GameFunction } from "@virtuals-protocol/game";
-declare class TelegramPluginDev extends TelegramPlugin {
-    constructor(options: ConstructorParameters<typeof TelegramPlugin>[0]);
-    get generateRiddleFunction(): GameFunction<[]>;
-}
-export default TelegramPluginDev;
